9752b38567b83180c1ba0664428e821dee998f5c
iontrxne <maksimus.kozhaev@mail.ru>
2024-04-28 19:46:51 +0300

    app is ready to be filled out

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const path = require('path');
 const { PORT } = require('./config');
 const { cors } = require('./middlewares/cors');
 const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
+const gamesRouter = require('./routes/games');
+const categoriesRouter = require('./routes/categories');
+const connectToDatabase = require('./database/connect');
 
 const app = express();
+
+connectToDatabase();
 
 app.use(
   cors,
   bodyParser.json(),
   express.static(path.join(__dirname, 'public')),
+  usersRouter,
+  gamesRouter,
+  categoriesRouter
 )
 
 app.listen(PORT, () => {
