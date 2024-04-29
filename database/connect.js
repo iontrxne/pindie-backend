738463ed0d8f21cd01ee22a4540caecfe76217dc
iontrxne <maksimus.kozhaev@mail.ru>
2024-04-29 13:18:20 +0300

    add mongoDB schema models

--- database/connect.js
+++ database/connect.js
@@ -4,7 +4,7 @@ const DB_URL = 'mongodb://localhost:27017/pindie';
 async function connectToDatabase() {
   try {
     await mongoose.connect(DB_URL);
-    console.log('Успешно подключились к MongoDB')
+    console.log('Успешное подключение к MongoDB')
   }
   catch (err) {
     console.log('При подключении MongoDB возникла ошибка')
