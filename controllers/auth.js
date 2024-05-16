cc3a71e8610336ab254629940cac6e67747e966d
iontrxne <maksimus.kozhaev@mail.ru>
2024-05-16 23:10:39 +0300

    upd login and add find by category

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,17 +5,7 @@ const login = (req, res) => {
   const { email, password } = req.body;
 
   users
-    .findOne({ email }).then(user => {
-        if (!user) {
-          return Promise.reject(new Error("Неправильные почта или пароль"));
-        }
-        return bcrypt.compare(password, user.password).then(matched => {
-          if (!matched) {
-            return Promise.reject(new Error("Неправильные почта или пароль"));
-          }
-          return user;
-        });
-      })
+    .findUserByCredentials(email, password)
     .then((user) => {
       res
         .status(200)
