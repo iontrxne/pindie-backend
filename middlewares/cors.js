f578527fc89736a84f5153782b6c3ebeb3568a97
iontrxne <maksimus.kozhaev@mail.ru>
2024-05-21 21:46:40 +0300

    upd cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,14 @@
-const PORT = require('../config');
-
-const allowedCors = [
-  "http://localhost:3000",
-  `http://localhost:${PORT}`,
-];
+const allowedCors = ["https://front.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  };
-
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"); 
-
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
-};
+}
 
-module.exports = cors;
+module.exports = cors; 
